721f2c8b9f348bbe35cdbf0d94b26553aa7f1020
Prikshit Gautam <pgautamlinkedin@gmail.com>
2026-04-07 09:41:17 +0530

    use raw github url for achievement image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -68,7 +68,7 @@ function About() {
                 transition={{ delay: 0.3, duration: 0.6 }}
               >
                 <img
-                  src="/achievement.jpeg"
+                  src="https://raw.githubusercontent.com/prikshitgautam27/portfolio/main/public/achievement.jpeg"
                   alt="Academic Excellence Award Ceremony"
                   className="w-full rounded-lg"
                 />
